93e8f26c6e49e8be72331279c04c1928089703c3
Matheus Folle <mathfolle@icloud.com>
2025-05-20 14:21:05 -0300

    cria index.php com listagem de sneakers

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'data.php';
+//include 'includes/header.php';
+?>
+
+<h1>void kicks catalogue</h1>
+
+<?php foreach ($sneakers as $item): ?>
+    <div class="sneaker-box">
+        <img src="<?= $item['image'] ?>" alt="<?= $item['name'] ?>" width="">
+        <h2><?= $item['name'] ?></h2>
+        <p><?= $item['cat'] ?></p>
+        <a href="details.php?id=<?= $item['id'] ?>">See more</a>
+    </div>
+    <?php endforeach; ?>
+
+    <?php include 'includes/footer.php'; ?> 
