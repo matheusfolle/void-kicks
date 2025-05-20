581ec4a850fe902bc7789e6cd34bbd1f59a7d601
Matheus Folle <mathfolle@icloud.com>
2025-05-20 14:00:51 -0300

    cria array de sneakers em dados.php

---
+++ dados.php
@@ -0,0 +1,45 @@
+$sneakers = [
+    [
+        "id" => 1,
+        "nome" => "Nike Blazer City Low XS",
+        "categoria" => "Casual",
+        "imagem" => "https://images.stockx.com/images/Nike-Blazer-City-Low-LX-Black-White-W.jpg?fit=fill&amp;bg=FFFFFF&amp;w=576&amp;h=384&amp;q=57&amp;dpr=2&amp;trim=color&amp;updated_at=1631740690",
+        "descricao" => "Tênis estiloso preto e branco."
+    ],
+    [
+        "id" => 2,
+        "nome" => "Adidas Yeezy Boost 700",
+        "categoria" => "Futuristico",
+        "imagem" => "https://images.stockx.com/360/adidas-Yeezy-Wave-Runner-700-Solid-Grey/Images/adidas-Yeezy-Wave-Runner-700-Solid-Grey/Lv2/img01.jpg?w=576&amp;q=57&amp;dpr=2&amp;updated_at=1635255057&amp;h=384",
+        "descricao" => "Clássico da Adidas com vibe Kanye West."
+    ],
+    [
+        "id" => 3,
+        "nome" => "Puma Speedcat OG",
+        "categoria" => "Casual",
+        "imagem" => "https://images.stockx.com/360/Puma-Speedcat-OG-Black-White/Images/Puma-Speedcat-OG-Black-White/Lv2/img01.jpg?w=576&amp;q=60&amp;dpr=1&amp;updated_at=1720531405&amp;h=384",
+        "descricao" => "Clássico desde as pistas."
+    ],
+    [
+        "id" => 4,
+        "nome" => "Nike Air Max 95 Corteiz Black Tour Yellow",
+        "categoria" => "Sport",
+        "imagem" => "https://images.stockx.com/360/Nike-Air-Max-95-Corteiz-Black-Tour-Yellow/Images/Nike-Air-Max-95-Corteiz-Black-Tour-Yellow/Lv2/img01.jpg?w=576&amp;q=57&amp;dpr=2&amp;updated_at=1746800574&amp;h=384",
+        "descricao" => "Impecável colaboração entre Corteiz e Nike."
+    ],
+    [
+        "id" => 5,
+        "nome" => "New Balance 2002R Triple Black Suede",
+        "categoria" => "Sport",
+        "imagem" => "https://images.stockx.com/360/New-Balance-2002R-Triple-Black-Suede/Images/New-Balance-2002R-Triple-Black-Suede/Lv2/img01.jpg?w=576&amp;q=57&amp;dpr=2&amp;updated_at=1738941433&amp;h=384",
+        "descricao" => "Sporting no melhor nível."
+    ],
+    [
+        "id" => 6,
+        "nome" => "ASICS Gel Kayano 14 Cream Black Metallic Plum",
+        "categoria" => "Sport",
+        "imagem" => "https://images.stockx.com/360/ASICS-Gel-Kayano-14-Cream-Black-Metallic-Plum/Images/ASICS-Gel-Kayano-14-Cream-Black-Metallic-Plum/Lv2/img01.jpg?w=576&amp;q=57&amp;dpr=2&amp;updated_at=1686313362&amp;h=384",
+        "descricao" => "Sporting ainda em melhor nível."
+    ],
+  
+];
